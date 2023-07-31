6dba5559fc714a6a72d194fdadb4f19fdb0c71f3
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2023-07-31 12:28:18 +0200

    test: Updated GalleryComponent spec

--- projects/ontimize-web-ngx-gallery/src/lib/components/gallery/o-gallery.component.spec.ts
+++ projects/ontimize-web-ngx-gallery/src/lib/components/gallery/o-gallery.component.spec.ts
@@ -1,7 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { GalleryComponent } from './o-gallery.component';
+import { GalleryImage, GalleryOptions } from '../../models';
 import { OGalleryModule } from '../../ontimize-web-ngx-gallery.module';
+import { GalleryComponent } from './o-gallery.component';
+
 
 describe('GalleryComponent', () => {
   let component: GalleryComponent;
@@ -9,13 +13,21 @@ describe('GalleryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [GalleryComponent],
-      imports: [OGalleryModule]
+      imports: [BrowserModule, CommonModule, OGalleryModule],
+      declarations: [GalleryComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(GalleryComponent);
     component = fixture.componentInstance;
+    component.images = [
+      new GalleryImage({
+        small: 'assets/1-small.jpg',
+        medium: 'assets/1-medium.jpg',
+        big: 'assets/1-big.jpg'
+      })
+    ];
+    component.options = [new GalleryOptions({})];
     fixture.detectChanges();
   });
 
