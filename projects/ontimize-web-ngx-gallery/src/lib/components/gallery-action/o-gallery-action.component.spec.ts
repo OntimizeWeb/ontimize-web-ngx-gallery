a9d1944a9bf8b8a13f3cfc1c9bb60d28f18a9625
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2023-07-31 09:31:20 +0200

    test: Added GalleryComponent and GalleryActionComponent

--- projects/ontimize-web-ngx-gallery/src/lib/components/gallery-action/o-gallery-action.component.spec.ts
+++ projects/ontimize-web-ngx-gallery/src/lib/components/gallery-action/o-gallery-action.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { GalleryComponent } from './o-gallery.component';
+import { GalleryActionComponent } from './o-gallery-action.component';
 import { OGalleryModule } from '../../ontimize-web-ngx-gallery.module';
 
-describe('GalleryComponent', () => {
-  let component: GalleryComponent;
-  let fixture: ComponentFixture<GalleryComponent>;
+describe('GalleryActionComponent', () => {
+  let component: GalleryActionComponent;
+  let fixture: ComponentFixture<GalleryActionComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [GalleryComponent],
+      declarations: [GalleryActionComponent],
       imports: [OGalleryModule]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(GalleryComponent);
+    fixture = TestBed.createComponent(GalleryActionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
