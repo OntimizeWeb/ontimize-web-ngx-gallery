9e3653ea05f6a81a5b01e17baf15e2c6080c6f79
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2023-11-15 19:08:29 +0100

    Updated `GalleryLayout` with `ThumbnailsLeft` and `ThumbnailsRight` constants

--- projects/ontimize-web-ngx-gallery/src/lib/models/gallery-layout.model.ts
+++ projects/ontimize-web-ngx-gallery/src/lib/models/gallery-layout.model.ts
@@ -1,4 +1,6 @@
 export class GalleryLayout {
   static ThumbnailsTop = 'thumbnails-top';
   static ThumbnailsBottom = 'thumbnails-bottom';
+  static ThumbnailsLeft = 'thumbnails-left';
+  static ThumbnailsRight = 'thumbnails-right';
 }
