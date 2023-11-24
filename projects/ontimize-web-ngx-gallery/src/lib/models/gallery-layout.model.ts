d16c33f7eb3e8d7039535a97f46b0f47ff5dd652
Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
2023-11-24 12:07:41 +0100

    Input `layout`  accepts `left` or `right` value (#129)
    * Updated `GalleryLayout` with `ThumbnailsLeft` and `ThumbnailsRight` constants
    
    * Added new input in o-gallery-thumbnails
    
    * Adapted getThumnailTop and getThumbnailLeft methods
    
    * Refactor code
    
    * Set width when thumbanails are left or right
    
    * New arrows styles when layout is left or right
    
    * Improved top or botton arrows styles
    
    * Improved positioning when layout === right or layout===left
    
    * Updated changeThumbPosition
    
    * New type GalleryLayouts
    
    * Improved arrows styles
    
    * Fixed imagePercent does not work with thumbnails in position left or right
    
    * New method moveBotton, moveTop, canMoveTop, canMoveBottom
    
    * New methods `moveThumbnailsTop`,, `moveThumbnailsBottom`, `canMoveThumbnailsTop`, `canMoveThumbnailsBottom`, `changeThumbnailsColumns`, `changeThumbnailsRows`
    
    * Set default values in variables thumbnailsColumns and thumbnailsRows
    
    * New styles arrows

--- projects/ontimize-web-ngx-gallery/src/lib/models/gallery-layout.model.ts
+++ projects/ontimize-web-ngx-gallery/src/lib/models/gallery-layout.model.ts
@@ -1,4 +1,9 @@
 export class GalleryLayout {
   static ThumbnailsTop = 'thumbnails-top';
   static ThumbnailsBottom = 'thumbnails-bottom';
+  static ThumbnailsLeft = 'thumbnails-left';
+  static ThumbnailsRight = 'thumbnails-right';
+
+
 }
+export type GalleryLayouts = 'thumbnails-top' | 'thumbnails-bottom' | 'thumbnails-left' | 'thumbnails-right';
